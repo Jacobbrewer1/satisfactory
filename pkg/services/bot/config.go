66ff030bb265eb1cb36be1a6ff6eab1de8a61645
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-09-23 17:52:52 +0100

    feat(credentials): adding server credentials command (#3)
    adding server credentials

--- pkg/services/bot/config.go
+++ pkg/services/bot/config.go
@@ -3,15 +3,21 @@ package bot
 import "github.com/bwmarrin/discordgo"
 
 const (
-	serverInfoCmdID = "server-info"
+	serverInfoCmdID        = "server-info"
+	serverCredentialsCmdID = "server-credentials"
 )
 
 var (
 	commands = []*discordgo.ApplicationCommand{
 		{
 			Name:        serverInfoCmdID,
-			Description: "Server Info",
 			Type:        discordgo.ChatApplicationCommand,
+			Description: "Server Info",
+		},
+		{
+			Name:        serverCredentialsCmdID,
+			Type:        discordgo.ChatApplicationCommand,
+			Description: "Server Credentials",
 		},
 	}
 )
