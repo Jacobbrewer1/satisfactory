da216e63ecfd370eec73bedcc77ab3d1a810eaf6
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-09-24 13:51:41 +0100

    Delete temp.sh

--- temp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -s --insecure https://192.168.1.253:7777/api/v1 --header 'Authorization: Bearer ewoJInBsIjogIkFQSVRva2VuIgp9.9F4E3195CE4C3DD165790189574D3D34828A837FBCBE04485F961F9A8FED6ADF4F0386F01C5119F3FB1D3136F59D165A255955475E0138FB3B2CF52598671D3A' --header 'Content-Type: application/json' --data '{
-    "function": "QueryServerState",
-    "data": {
-        "clientCustomData": ""
-    }
-}' | jq -c
